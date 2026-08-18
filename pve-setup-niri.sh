f1301f39dff3eeeb8b596685014972e5ba734f74
lee <sleechengn@live.cn>
2026-08-18 08:38:27 +0800

    sav

--- pve-setup-niri.sh
+++ pve-setup-niri.sh
@@ -36,3 +36,4 @@ fi
 
 systemctl --user -M sa@ daemon-reload
 systemctl --user -M sa@ enable --now pipewire pipewire-pulse wireplumber
+su sa -c "gsettings set org.gnome.desktop.interface color-scheme prefer-dark"
